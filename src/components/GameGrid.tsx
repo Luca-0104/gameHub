d2a014f4fcffff6da36ef2b35bd216ca535a5601
LiuZhe <lzhe72537@gmail.com>
2024-01-19 22:45:12 -0500

    optimize the game card container

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,6 +3,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   // The customized hook for fetching the games
@@ -13,9 +14,11 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10}>
-        {isLoading && skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+        {isLoading && skeletons.map((skeleton) => <GameCardContainer><GameCardSkeleton key={skeleton} /></GameCardContainer>)}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+              <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
