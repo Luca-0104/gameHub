38f29f434aa78c36044d714b8774a5ea60ea1e0c
LiuZhe <lzhe72537@gmail.com>
2024-01-28 21:15:33 -0500

    dynamic game heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -38,6 +39,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main" paddingX={5}>
+        <GameHeading gameQuery={gameQuery} />
         <HStack spacing={5} marginBottom={8}>
           <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
           <SortSelector sortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
