8e43db97f1006f9837545830bd4eba48309dd494
LiuZhe <lzhe72537@gmail.com>
2024-01-28 22:09:17 -0500

    fix problems with placeholder image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,4 @@
-import noImage from '../assets/placeholder.jpg'
+import noImage from '../assets/no-image-placeholder.webp'
 
 const getCroppedImageUrl = (url: string) => {
     if (!url) return noImage;
