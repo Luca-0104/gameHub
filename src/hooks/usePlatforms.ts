d514ab532ec61c44cd7cc86027e512e6ac35c2be
LiuZhe <lzhe72537@gmail.com>
2024-02-01 01:58:05 -0500

    update platform hook using react query with cache

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,22 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import { FetchResponse } from "./useData";
+import apiClient from "../services/api-client";
+import platforms from "../data/platforms";
 
 interface Platform {
-    id: number;
-    name: string;
-    slug: string;
+  id: number;
+  name: string;
+  slug: string;
 }
 
-const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+const usePlatform = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Platform>>("/platforms/lists/parents").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms }
+  });
+
 
 export default usePlatform;
