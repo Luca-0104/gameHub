90d57506226cd08d97580bfd3ae849993fdbaff0
LiuZhe <lzhe72537@gmail.com>
2024-02-04 22:34:17 -0500

    new hook useGenre

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -4,15 +4,14 @@ import { Heading } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import usePlatforms from '../hooks/usePlatforms';
 import usePlatform from '../hooks/usePlatform';
+import useGenre from '../hooks/useGenre';
 
 interface Props {
     gameQuery: GameQuery;
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-    const { data : genres } = useGenres();
-    const genre = genres?.results.find(g => g.id === gameQuery.genreId);
-
+    const genre = useGenre(gameQuery.genreId);
     const platform = usePlatform(gameQuery.platformId);
 
     const heading = `${platform?.name || ""} ${genre?.name || ""} Games`;
---
+++ src/hooks/useGenre.ts
@@ -0,0 +1,9 @@
+import useGenres from "./useGenres";
+
+const useGenre = (id?: number) => {
+    const { data : genres } = useGenres();
+    const genre = genres?.results.find(g => g.id === id);
+    return genre;
+}
+
+export default useGenre;
