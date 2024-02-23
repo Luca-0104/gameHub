3e486bdaf0c9f7475d25ce4ed246ecc6b57d0ab2
LiuZhe <lzhe72537@gmail.com>
2024-02-23 00:20:58 -0500

    implemented the basic game details page

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -1,16 +1,24 @@
-import { Box } from '@chakra-ui/react'
-import { ReactNode } from 'react'
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 interface Props {
-    children: ReactNode
+  children: ReactNode;
 }
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="100%" borderRadius={10} overflow={'hidden'}>
-        { children }
+    <Box
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+      width="100%"
+      borderRadius={10}
+      overflow={"hidden"}
+    >
+      {children}
     </Box>
-  )
-}
+  );
+};
 
-export default GameCardContainer
+export default GameCardContainer;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,28 +1,34 @@
 import axios, { AxiosRequestConfig } from "axios";
 
 export interface FetchResponse<T> {
-    count: number;
-    next: string | null;
-    results: T[];
+  count: number;
+  next: string | null;
+  results: T[];
 }
 
 const axiosInstance = axios.create({
-    baseURL: "https://api.rawg.io/api",
-    params: {
-        key: "e5740090d2f547ee8af54db8b89f29c7"
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "e5740090d2f547ee8af54db8b89f29c7",
+  },
+});
 
 export default class APIClient<T> {
-    endpoint: string;
+  endpoint: string;
 
-    constructor(endpoint: string) {
-        this.endpoint = endpoint;
-    }
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
 
-    getAll = (config : AxiosRequestConfig) => {
-        return axiosInstance
-                .get<FetchResponse<T>>(this.endpoint, config)
-                .then(res => res.data)
-    }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+
+  get = (id: number | string) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
 }
