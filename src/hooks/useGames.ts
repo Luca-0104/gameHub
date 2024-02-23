a997a4decfeb83c380ef731e6ee139b255d1756b
LiuZhe <lzhe72537@gmail.com>
2024-02-23 00:25:25 -0500

    move all the entity interface into a entities dir

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,20 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import ms from "ms";
 import APIClient, { FetchResponse } from "../services/api-client";
-import { Platform } from "./usePlatforms";
 import useGameQueryStore from "../store";
-
-export interface Game {
-  id: number;
-  name: string;
-  slug: string;
-  description_raw: string;
-  background_image: string;
-  parent_platforms: { platform: Platform }[];
-  metacritic: number;
-  rating_top: number;
-  rating: number;
-}
+import { Game } from "../entities/Game";
 
 const apiClient = new APIClient<Game>("/games");
 
