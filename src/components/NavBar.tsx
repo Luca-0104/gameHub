ef0e80944593556d105a3f163b5c4def34bad9b3
LiuZhe <lzhe72537@gmail.com>
2024-02-24 19:16:48 -0500

    fix the bug of cannot search in the detail page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
-import { HStack, Image } from '@chakra-ui/react'
+import { HStack, Image } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
 import logo from '../assets/logo.svg';
 import ColorModeSwitch from './ColorModeSwitch';
 import SearchInput from './SearchInput';
@@ -6,7 +7,9 @@ import SearchInput from './SearchInput';
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-        <Image src={logo} boxSize="60px" margin="5px"/>
+        <Link to={'/'}>
+          <Image src={logo} boxSize="60px" margin="5px" objectFit={'cover'}/>
+        </Link>
         <SearchInput/>
         <ColorModeSwitch/>
     </HStack>
